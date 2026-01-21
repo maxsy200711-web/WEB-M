de1b93d3d6c06c0b0c0685f698d65387177fe2e5
maxsy200711-web <maxsy200711@gmail.com>
2026-01-21 08:59:30 +0700

    21.01

--- app/models/db.js
+++ app/models/db.js
@@ -1,12 +1,9 @@
-const dbConfig = require("../config/db.config");
 const mysql = require("mysql");
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB
 });
-
-module.exports = connection;
